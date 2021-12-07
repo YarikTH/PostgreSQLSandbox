98853233ee61ef3a689d00f1631ef392b16a18cd
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-12-07 23:25:15 +0300

    Add initial code

---
+++ library.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sstream>
+#include <pqxx/pqxx>
+
+namespace
+{
+    const std::string dbname = "Wtf";
+    const std::string dbuser = "Wtf";
+    const std::string dbpass = "Wtf";
+}
+
+int main()
+{
+    std::cout << "Test" << std::endl;
+    
+    try
+    {
+        std::ostringstream ss;
+        ss << "dbname = " << dbname << " user = " << dbuser << " password = " << dbuser << " hostaddr = 127.0.0.1 port = 5432";
+        
+        pqxx::connection connection( ss.str() );
+        if( connection.is_open() )
+        {
+            std::cout << "Opened database successfully: " << connection.dbname() << std::endl;
+        }
+        else
+        {
+            std::cout << "Can't open database" << std::endl;
+            return 1;
+        }
+        connection.close();
+    } catch( const std::exception& e )
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef POSTRGREETEST_LIBRARY_H
+#define POSTRGREETEST_LIBRARY_H
+
+void hello();
+
+#endif //POSTRGREETEST_LIBRARY_H
